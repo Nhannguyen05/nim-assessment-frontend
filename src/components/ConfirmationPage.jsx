e3cf890d8ffdeee3734fee3d9cf2522a195c9a0b
Nhan Nguyen <66826103+Nhannguyen05@users.noreply.github.com>
2023-12-14 23:46:44 -0700

    Create ConfirmationPage.jsx
    ConfirmationPage should connect to the server and retrieve an order based
    on the id (the endpoint lives at “/api/orders/<id>” and e should pass the order to the OrderConfirmation
    component as a prop named “order”

---
+++ src/components/ConfirmationPage.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import OrderConfirmation from "./OrderConfirmation";
+import styles from "./styles/ConfirmationPage.module.css";
+
+function ConfirmationPage() {
+  const [order, setOrder] = useState(null);
+  const { id: orderId } = useParams();
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        const response = await fetch(`/api/orders/${orderId}`);
+        const data = await response.json();
+        setOrder(data);
+      } catch (error) {
+        console.error("Error fetching order:", error);
+      }
+    };
+
+    fetchOrder();
+  }, [orderId]);
+
+  if (!order) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.confirmation}>
+        <OrderConfirmation order={order} />
+      </div>
+    </div>
+  );
+}
+
+export default ConfirmationPage;
