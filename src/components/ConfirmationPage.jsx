219dc8084bb7014c73dce4f5213b2ad6c369d8e6
Nhan Nguyen <66826103+Nhannguyen05@users.noreply.github.com>
2023-12-14 23:59:43 -0700

    Update ConfirmationPage.jsx
    fetch data from sampleTestData for testing and styling

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import OrderConfirmation from "./OrderConfirmation";
+import { testOrder } from "../sampleTestData";
 import styles from "./styles/ConfirmationPage.module.css";
 
 function ConfirmationPage() {
@@ -10,9 +11,9 @@ function ConfirmationPage() {
   useEffect(() => {
     const fetchOrder = async () => {
       try {
-        const response = await fetch(`/api/orders/${orderId}`);
-        const data = await response.json();
-        setOrder(data);
+        // const response = await fetch(`/api/orders/${orderId}`);
+        // const data = await response.json();
+        setOrder(testOrder);
       } catch (error) {
         console.error("Error fetching order:", error);
       }
